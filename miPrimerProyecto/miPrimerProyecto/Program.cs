e24009f9589693b697d30219cf9da6be6e92247e
Luis Hernandez <sarco_luis@hotmail.com>
2024-07-30 08:32:27 -0600

    Uso de la estructura repetitiva for, desde hasta incrementeo

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -12,13 +12,15 @@ namespace miPrimerProyecto {
             //2. Switch ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
             string continuar = "s";
             while (continuar == "s") {
-                Console.WriteLine("*** MENU ***");
+                Console.WriteLine("\n\n *** MENU ***");
                 Console.WriteLine("1. Promedio Notas");
                 Console.WriteLine("2. Promedio Serie Numeros");
                 Console.WriteLine("3. Clasificacion edad");
-                Console.WriteLine("4. Salir");
+                Console.WriteLine("4. Tabla Multiplicar");
+                Console.WriteLine("5. Salir");
                 Console.Write("Opcion: ");
                 int opcion = int.Parse(Console.ReadLine());
+                Console.Clear();//limpiar la consola
                 switch (opcion) {
                     case 1://if(opcion==1)
                         promedio();
@@ -29,13 +31,23 @@ namespace miPrimerProyecto {
                     case 3://if(opcion==3)
                         clasificacionEdad();
                         break;
-                    case 4://if(opcion==4)
+                    case 4:
+                        tablaMultiplicar();
+                        break;
+                    case 5://if(opcion==5)
                         continuar = "n";
                         break;
                     default://else
                         Console.WriteLine("opcion incorrecta \n\n");
                         break;
                 }
+            }
+        }
+        static void tablaMultiplicar() {
+            Console.Write("Tabla: ");
+            int ntabla = int.Parse(Console.ReadLine());
+            for(int i=1; i<=10;  i++) {//i++ => i = i + 1
+                Console.WriteLine("{0}x{1}={2}", ntabla, i, ntabla*i);
             }
         }
         static void promedio() {
