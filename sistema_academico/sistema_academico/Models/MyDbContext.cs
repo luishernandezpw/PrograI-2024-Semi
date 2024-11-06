610a58e81d88c4dd2d89cbb2c09adbf97ef57cf8
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2024-11-06 10:30:57 -0600

    creacion del formulario docentes

--- sistema_academico/sistema_academico/Models/MyDbContext.cs
+++ sistema_academico/sistema_academico/Models/MyDbContext.cs
@@ -12,10 +12,12 @@ namespace sistema_academico.Models
 
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
         public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Docente> Docentes { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Alumno>().HasKey(e => e.idAlumno);
+            modelBuilder.Entity<Docente>().HasKey(e => e.idDocente);
         }
     }
 }
