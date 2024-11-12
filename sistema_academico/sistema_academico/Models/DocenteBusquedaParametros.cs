3aa86d8d8132ac3a4d3177ad4d9881d951f2cf01
Luis Hernandez <sarco_luis@hotmail.com>
2024-11-12 08:16:04 -0600

    Terminacion del formulario de docentes + la busqueda

---
+++ sistema_academico/sistema_academico/Models/DocenteBusquedaParametros.cs
@@ -0,0 +1,6 @@
+﻿namespace sistema_academico.Models {
+    public class DocenteBusquedaParametros {
+        public string? nombre { get; set; }
+        public string? codigo { get; set; }
+    }
+}
--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ff9fa5c88a9273712a262c7d8b2bc2341d03eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610a58e81d88c4dd2d89cbb2c09adbf97ef57cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
